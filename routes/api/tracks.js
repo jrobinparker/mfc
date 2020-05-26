7bba690dd5e82a76977d48215b3d550483e7dd94
Jeremy Parker <jrobinparker@gmail.com>
2020-05-26 10:34:21 -0700

    Added get all tracks route

--- routes/api/tracks.js
+++ routes/api/tracks.js
@@ -44,4 +44,17 @@ router.post('/', [ auth, [
     }
   });
 
+// @route GET api/tracks
+// @desc Get all tracks
+// @access Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const tracks = await Track.find().sort({ date: -1 });
+    res.json(tracks);
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
