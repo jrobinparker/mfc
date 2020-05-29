b134bd11e604a41acfca231b6832c8504e64e30b
Jeremy Parker <jrobinparker@gmail.com>
2020-05-29 10:23:17 -0700

    Added landing page redirect to dashboard when user is logged in

--- client/src/components/landing/Main.js
+++ client/src/components/landing/Main.js
@@ -1,13 +1,29 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import Nav from './nav/Nav';
 import Hero from './hero/Hero';
+import { connect } from 'react-redux';
+import { isAuthenticated } from '../../actions/auth';
 
-const Main = () => {
+const Main = ({ isAuthenticated }) => {
+
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
   return (
       <Fragment>
         <Hero />
       </Fragment>
   )
-}
+};
 
-export default Main;
+Main.propTypes = {
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth
+})
+
+export default connect(mapStateToProps)(Main);
